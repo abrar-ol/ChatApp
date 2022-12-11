ec841fd2372b773fbebefba3d72eb0a1e133509b
abrar_ol <abrar_ol@hotmail.com>
2022-12-11 10:45:26 +0300

    Navbar UI

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import "./navbar.css";
+import Notification from "../../img/notification.svg";
+import Message from "../../img/message.svg";
+import Settings from "../../img/settings.svg"
 
 const Navbar = () => {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">
+      <span className="logo">Abrar App</span>
+      <div className="icons">
+        <div className="icon">
+            <img src={Notification} className="iconImg" alt="Notification Icon" />
+            <div className="counter">2</div>
+        </div>
+        <div className="icon">
+            <img src={Message} className="iconImg" alt="Message Icon" />
+            <div className="counter">1</div>
+        </div>
+        <div className="icon">
+            <img src={Settings} className="iconImg" alt="Settings Icon" />
+            <div className="counter">2</div>
+        </div>
+      </div>
+    </div>
   )
 }
 

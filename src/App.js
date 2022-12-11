606eb670d27ac78f3cafc97b62ad45918d296321
abrar_ol <abrar_ol@hotmail.com>
2022-12-11 09:28:24 +0300

    Added initial login+navbar+card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-
+import {useState} from "react";
+import "./app.css";
 
 function App() {
-  return<>Hello World!</>;
+  const [username,setUsername]= useState("");
+  const [user,setUser]= useState("");
+  
+  return (
+    <div className="container">
+      <div className="login">
+        <input type="text" placeholder="Username" onChange={(e)=>{setUsername(e.target.value)}}></input>
+        <button onClick={()=>setUser(username)}>Login</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;

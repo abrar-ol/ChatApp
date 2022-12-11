1b63fb5cfd11867dd55d471aa29a1d7236f087fa
abrar_ol <abrar_ol@hotmail.com>
2022-12-11 09:56:07 +0300

    Added condition for navbar and card

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import {useState} from "react";
+import { useState } from "react";
+import Navbar from "./components/navbar/Navbar";
+import Card from "./components/card/Card";
+
 import "./app.css";
 
 function App() {
-  const [username,setUsername]= useState("");
-  const [user,setUser]= useState("");
-  
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+
   return (
     <div className="container">
-      <div className="login">
-        <input type="text" placeholder="Username" onChange={(e)=>{setUsername(e.target.value)}}></input>
-        <button onClick={()=>setUser(username)}>Login</button>
-      </div>
+      {user ? (
+        <>
+          <Navbar />
+          <Card />
+          <span className="username">
+            {user}
+          </span>
+        </>
+      ) : (
+        <div className="login">
+          <input
+            type="text"
+            placeholder="Username"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          ></input>
+          <button onClick={() => setUser(username)}>Login</button>
+        </div>
+      )}
     </div>
   );
 }
